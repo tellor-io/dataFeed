f39f43bac610c67d14a45e6ecf17397ee08a4d80
raynharr <rhill@tellor.io>
2023-05-05 09:49:14 -0500

    tipelip

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -169,7 +169,7 @@ export const decodingAutopayMiddleware = (autopayEvents) => {
     let queryData
     let finalQueryData
     event.interval = event._interval ? `${(event._interval / 60 / 60)} hours` : 'One Time Tip'
-    event.tip = event._reward ? web3.utils.fromWei(event._reward) + "..." + ' TRB' : web3.utils.fromWei(event._amount).slice(0,5) + ' TRB' 
+    event.tip = event._reward ? web3.utils.fromWei(event._reward) + ' TRB' : web3.utils.fromWei(event._amount).slice(0,6) + ' TRB' 
     event.startTime = getDate(event._startTime)
     event.window = event._window
     event.symbols = event._queryData
